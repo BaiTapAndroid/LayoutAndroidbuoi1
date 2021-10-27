c2fc14b47ed14a5371f2cccbe1aeadd86cf67597
leanhthanhtai <leanhthanhtai@gmail.com>
2021-10-27 09:34:51 +0700

    Layout01

---
+++ app/src/main/java/com/example/layout_tuan1_18093421_LeVanTai/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.layout_tuan1_18093421_LeVanTai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_3);
+    }
+}
